3234e4ce2029dafa844e10e55dde935d9afcb181
AliMotamedi7 <motamedi1560@gmail.com>
2024-04-15 15:00:32 +0330

    initial commit

---
+++ lib/model/map_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MapModel {
+  Color color;
+
+  MapModel(this.color);
+}
+
---
+++ lib/pages/Home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_maps/maps.dart';
+
+import '../model/map_model.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  MapShapeSource? _shapeSource;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _shapeSource =
+        const MapShapeSource.asset("assets/ir.json", shapeDataField: "name");
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SfMaps(layers: [MapShapeLayer(source: _shapeSource!)]),
+        ),
+      ),
+    );
+  }
+}
